65b4d3b2f4173a389a81b0de179d0590632c4a4a
(ю:】 <gardentreex@gmail.com>
2023-02-13 12:11:27 +0900

    :+1: Make firefox preferences

---
+++ firefox/user.js
@@ -0,0 +1,10 @@
+user_pref('browser.search.openintab', true);                            //検索結果を新規タブで表示する
+user_pref('browser.tabs.closeWindowWithLastTab', false);                //最後のタブを閉じる時にWindowを閉じない
+user_pref('browser.tabs.insertRelatedAfterCurrent', false);             //新規タブの追加を右端にする
+user_pref('browser.tabs.loadBookmarksInTabs', true);                    //ブックマークを新規タブで表示する
+user_pref('browser.tabs.selectOwnerOnClose', false);                    //新規タブにフォーカスを移さない
+user_pref('browser.tabs.warnOnCloseOtherTabs', false);                  //"他のタブをすべて閉じる"を確認しない
+user_pref('extensions.pocket.enabled', false);                          //pocketを無効にする
+user_pref('full-screen-api.warning.timeout', 0);                        //フルスクリーンの警告を非表示にする
+user_pref('media.autoplay.enabled', false);                             //動画の自動再生を無効にする
+user_pref('toolkit.legacyUserProfileCustomizations.stylesheets', true); //User Chromeを有効にする
